210cd6a9a46b23c42e4943d8ef74475075cc324c
Joel Terrell <falconreid@hotmail.com>
2020-06-27 08:35:04 -0600

    Added Readme explanation, made minor CSS and HTML changes

---
+++ README.mk
@@ -0,0 +1,12 @@
+Assignment 1 - Code Refactor 
+
+For this assignment we were supposed to clean up an existing site and make it conform to accessibility standards.
+
+To this end I:
+* Added Alt tags to images
+* Added semantic markup language to differentiate sections
+* Consolidated inefficient CSS
+* Made content larger for easier readabilty
+* Cleaned up appearance on page so article and aside are equal heights
+* Added button at bottom to return to the top
+* Identified Benefits section with header tag.
